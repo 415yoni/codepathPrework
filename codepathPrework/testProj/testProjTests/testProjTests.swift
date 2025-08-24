1e6b58caf248a04155f67fe38e3ec65ba892025b
415yoni <111541923+415yoni@users.noreply.github.com>
2025-08-24 17:15:48 -0400

    Add files via upload

---
+++ codepathPrework/testProj/testProjTests/testProjTests.swift
@@ -0,0 +1,17 @@
+//
+//  testProjTests.swift
+//  testProjTests
+//
+//  Created by Yonatan Simie on 8/24/25.
+//
+
+import Testing
+@testable import testProj
+
+struct testProjTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
